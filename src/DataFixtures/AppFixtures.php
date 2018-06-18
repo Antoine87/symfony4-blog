4f212a00b34979d69598eea38061c4326baa1c47
Antoine87 <antoine87@openmail.cc>
2018-06-18 22:53:47 +0200

    Added Post entity + fixtures + configured stof sluggable, timestampable

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Post;
+use Cocur\Slugify\Slugify;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+
+class AppFixtures extends Fixture
+{
+    /**
+     * @var \Faker\Generator
+     */
+    private $faker;
+
+    /**
+     * @var \Cocur\Slugify\Slugify
+     */
+    private $slugify;
+
+
+    public function __construct()
+    {
+        $this->faker = Factory::create();
+        $this->slugify = Slugify::create();
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $this->loadPosts($manager);
+    }
+
+    private function loadPosts(ObjectManager $manager): void
+    {
+        foreach ($this->getPostData() as [
+            $title, $slug, $summary, $content, $image, $publishedAt, $author, $createAt, $updatedAt, $deletedAt
+        ]) {
+            $post = new Post();
+            $post->setTitle($title);
+            $this->faker->boolean(10) && $post->setSlug($slug);
+            $post->setSummary($summary);
+            $post->setContent($content);
+            $post->setImage($image);
+            $this->faker->boolean(80) && $post->setPublishedAt($publishedAt);
+            $post->setAuthor($author);
+            if ($this->faker->boolean(80)) {
+                $post->setCreatedAt($createAt);
+                $post->setUpdatedAt($updatedAt);
+            }
+            $this->faker->boolean(10) && $post->setDeletedAt($deletedAt);
+
+            $manager->persist($post);
+        }
+        $manager->flush();
+    }
+
+    private function getPostData(): array
+    {
+        $posts = [];
+        foreach (range(1, 50) as $i) {
+            $posts[] = [
+                // $title, $slug, $summary, $content, $image, $publishedAt, $author, $createAt, $updatedAt, $deletedAt
+                $this->faker->unique()->word . ' ' . $this->faker->words($this->faker->randomDigit, true),
+                $this->faker->unique()->slug,
+                $this->faker->text(255),
+                $this->faker->realText,
+                '', // TODO image
+                $this->faker->dateTimeBetween('-6 months', 'now'),
+                $this->faker->userName, // TODO author
+                $this->faker->dateTimeBetween('-2 years', '-1 years'),
+                $this->faker->dateTimeBetween('-1 years', '-6 months'),
+                $this->faker->dateTimeBetween('-6 months', 'now')
+            ];
+        }
+        return $posts;
+    }
+}
---
+++ src/Entity/Post.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Gedmo\SoftDeleteable\Traits\SoftDeleteableEntity;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PostRepository")
+ * @ORM\Table(name="post")
+ */
+class Post
+{
+    use TimestampableEntity, SoftDeleteableEntity;
+
+    /**
+     * @var int
+     *
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @Gedmo\Slug(fields={"title"})
+     * @ORM\Column(type="string", unique=true)
+     */
+    private $slug;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $summary;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $image;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     */
+    private $author;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $publishedAt;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getSummary(): ?string
+    {
+        return $this->summary;
+    }
+
+    public function setSummary(?string $summary): self
+    {
+        $this->summary = $summary;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getPublishedAt(): ?\DateTimeInterface
+    {
+        return $this->publishedAt;
+    }
+
+    public function setPublishedAt(?\DateTimeInterface $publishedAt): self
+    {
+        $this->publishedAt = $publishedAt;
+
+        return $this;
+    }
+}
